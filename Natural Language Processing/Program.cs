8cfb09fb1f0cb06b2cb459a3ebe06cef796f852b
Danil Tkachenko <danil.tkachenko@getmore.cz>
2023-03-13 23:15:02 +0100

    Configure settings of project

--- Natural Language Processing/Program.cs
+++ Natural Language Processing/Program.cs
@@ -3,58 +3,75 @@ using Azure.AI.TextAnalytics;
 using Azure;
 using System.Reflection.Metadata;
 using System.Reflection;
-
-string endpoint = "https://nlpservicehub.cognitiveservices.azure.com/";
-
-string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"ApiKey.txt");
-
-List<string>? modifiedPath = path?.Split('\\').ToList();
-foreach (var item in modifiedPath.ToList())
+using MLModelSentimentAnalysis;
+internal class Program
 {
-	if (item == "bin")
+	private static void Main(string[] args)
 	{
-		modifiedPath.Remove(item);
-		continue;
-	}
-	if (item == "Debug")
-	{
-		modifiedPath.Remove(item);
-		continue;
-	}
-	if (item == "net7.0")
-	{
-		modifiedPath.Remove(item);
-		continue;
-	}
+		MLModel.ModelInput sampleData = new MLModel.ModelInput()
+		{
+			//_1467810369 = 1.467811E+09F,
+			//Mon_Apr_06_22_19_45_PDT_2009 = @"Mon Apr 06 22:19:53 PDT 2009",
+			//NO_QUERY = @"NO_QUERY",
+			//__TheSpecialOne_ = @"mattycus",
+			__switchfoot_http___twitpic_com_2y1zl___Awww__that_s_a_bummer___You_shoulda_got_David_Carr_of_Third_Day_to_do_it___D = @"I love you so much you",
+		};
 
+		// Make a single prediction on the sample data and print results
+		var predictionResult = MLModel.Predict(sampleData);
+	}
 }
+//string endpoint = "https://nlpservicehub.cognitiveservices.azure.com/";
 
-//for (int i = 0; i < modifiedPath.Count; i++)
+//string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"ApiKey.txt");
+
+//List<string>? modifiedPath = path?.Split('\\').ToList();
+//foreach (var item in modifiedPath.ToList())
 //{
-//	if (modifiedPath[i] != "ApiKey.txt")
+//	if (item == "bin")
 //	{
-//		modifiedPath[i] = modifiedPath[i] + "\\";
-
+//		modifiedPath.Remove(item);
+//		continue;
 //	}
+//	if (item == "Debug")
+//	{
+//		modifiedPath.Remove(item);
+//		continue;
+//	}
+//	if (item == "net7.0")
+//	{
+//		modifiedPath.Remove(item);
+//		continue;
+//	}
+
 //}
-string apiKey = File.ReadAllText(string.Join("\\",modifiedPath));
-var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
 
-string document = @"I hate this movie..";
+////for (int i = 0; i < modifiedPath.Count; i++)
+////{
+////	if (modifiedPath[i] != "ApiKey.txt")
+////	{
+////		modifiedPath[i] = modifiedPath[i] + "\\";
 
-try
-{
-	Response<DocumentSentiment> response = client.AnalyzeSentiment(document);
-	DocumentSentiment docSentiment = response.Value;
+////	}
+////}
+//string apiKey = File.ReadAllText(string.Join("\\",modifiedPath));
+//var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
 
-	Console.WriteLine($"Sentiment was {docSentiment.Sentiment}, with confidence scores: ");
-	Console.WriteLine($"  Positive confidence score: {docSentiment.ConfidenceScores.Positive}.");
-	Console.WriteLine($"  Neutral confidence score: {docSentiment.ConfidenceScores.Neutral}.");
-	Console.WriteLine($"  Negative confidence score: {docSentiment.ConfidenceScores.Negative}.");
-}
-catch (RequestFailedException exception)
-{
-	Console.WriteLine($"Error Code: {exception.ErrorCode}");
-	Console.WriteLine($"Message: {exception.Message}");
-}
+//string document = @"I hate this movie..";
+
+//try
+//{
+//	Response<DocumentSentiment> response = client.AnalyzeSentiment(document);
+//	DocumentSentiment docSentiment = response.Value;
+
+//	Console.WriteLine($"Sentiment was {docSentiment.Sentiment}, with confidence scores: ");
+//	Console.WriteLine($"  Positive confidence score: {docSentiment.ConfidenceScores.Positive}.");
+//	Console.WriteLine($"  Neutral confidence score: {docSentiment.ConfidenceScores.Neutral}.");
+//	Console.WriteLine($"  Negative confidence score: {docSentiment.ConfidenceScores.Negative}.");
+//}
+//catch (RequestFailedException exception)
+//{
+//	Console.WriteLine($"Error Code: {exception.ErrorCode}");
+//	Console.WriteLine($"Message: {exception.Message}");
+//}
 
